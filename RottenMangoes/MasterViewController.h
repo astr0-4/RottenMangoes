1e107ea02cc7e8bfd08508acf079bdf3e80e4504
Alex Stroulger <alexstroulger@gmail.com>
2015-07-01 11:23:41 -0400

    Initial Commit

---
+++ RottenMangoes/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  RottenMangoes
+//
+//  Created by Alex on 2015-07-01.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
